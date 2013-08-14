d2100496ee262c30ae47030269cfb2796e1ba784
Claudius Teodorescu <claudius.teodorescu@gmail.com>
2013-08-14 12:55:52 +0300

    Added 'transform-spec-to-html-format' mojo and unit test.

---
+++ kuberam-expath-plugin/src/main/java/ro/kuberam/maven/expathPlugin/mojos/TransformSpecToHtmlFormatMojo.java
@@ -0,0 +1,60 @@
+package ro.kuberam.maven.expathPlugin.mojos;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.dependencies;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.dependency;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.name;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
+
+import java.io.File;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "transform-spec-to-html-format")
+public class TransformSpecToHtmlFormatMojo extends AbstractExpathMojo {
+
+	@Parameter(required = true)
+	private String specId;
+
+	@Parameter(required = true)
+	private File specDir;
+
+	@Parameter(required = true)
+	private File outputDir;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		// create the outputDir directory
+		if (!outputDir.exists()) {
+			outputDir.mkdir();
+		}
+
+		// transform the spec
+		executeMojo(
+				plugin(groupId("org.codehaus.mojo"), artifactId("xml-maven-plugin"), version("1.0"),
+						dependencies(dependency("net.sf.saxon", "Saxon-HE", "9.4.0.7"))),
+				goal("transform"),
+				configuration(
+						element(name("forceCreation"), "true"),
+						element(name("transformationSets"),
+								element(name("transformationSet"),
+										element(name("dir"), specDir.getAbsolutePath()),
+										element(name("includes"), element(name("include"), specId + ".xml")),
+										element(name("stylesheet"),
+												this.getClass().getResource("/ro/kuberam/maven/expathPlugin/xmlspec.xsl").toString()),
+										element(name("outputDir"), outputDir.getAbsolutePath())))),
+				executionEnvironment(project, session, pluginManager));
+	}
+
+}
