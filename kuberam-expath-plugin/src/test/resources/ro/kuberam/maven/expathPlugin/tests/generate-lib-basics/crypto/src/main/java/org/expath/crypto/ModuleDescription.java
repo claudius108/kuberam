1493c3a15831be6ceb06d197756e6d10c6fe7bc5
Claudius Teodorescu <claudius.teodorescu@gmail.com>
2013-08-16 17:01:16 +0300

    Added @role for author, and changed the output dir.

--- kuberam-expath-plugin/src/test/resources/ro/kuberam/maven/expathPlugin/tests/generate-lib-basics/crypto/src/main/java/org/expath/crypto/ModuleDescription.java
+++ kuberam-expath-plugin/src/test/resources/ro/kuberam/maven/expathPlugin/tests/generate-lib-basics/crypto/src/main/java/org/expath/crypto/ModuleDescription.java
@@ -10,6 +10,6 @@ public class ModuleDescription {
       public final static String VERSION = "0.2.5";
       public final static String NAMESPACE_URI = "http://expath.org/ns/crypto";
       public final static String PREFIX = "crypto";
-      public final static String MODULE_NAME = "EXPath Cryptographic Module";
+      public final static String MODULE_NAME = "EXPath Cryptographic Library";
       public final static String MODULE_DESCRIPTION = "A module for providing cryptographic services";
 }
