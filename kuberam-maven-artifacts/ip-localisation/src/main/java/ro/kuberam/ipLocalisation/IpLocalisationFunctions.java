bfa73f159f170dcb023fe56a400f56f8ae72eae3
Claudius Teodorescu <claudius.teodorescu@gmail.com>
2013-11-04 15:18:06 +0100

    Added țip-localisationț artifact.

---
+++ kuberam-maven-artifacts/ip-localisation/src/main/java/ro/kuberam/ipLocalisation/IpLocalisationFunctions.java
@@ -0,0 +1,139 @@
+package ro.kuberam.ipLocalisation;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpLocalisationFunctions {
+
+	private static ClassLoader classLoader = IpLocalisationFunctions.class.getClassLoader();
+
+	private static long[] startRanges;
+	private static String[] countryIsoAlpha2Codes;
+	private static HashSet<String> languageTags;
+	private static Properties countryIsoAlpha3CodesProperties = new Properties();
+	private static Properties countryShortNamesGazetteerOrderEnUsProperties = new Properties();
+
+	private static Pattern pattern;
+	private static Matcher matcher;
+
+	private static String IPADDRESS_PATTERN = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+			+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+	static {
+		final InputStream startRangeIpsInputStream;
+		ObjectInputStream startRangeIpsObjectInputStream = null;
+		final InputStream countryAlpha2CodesInputStream;
+		ObjectInputStream countryAlpha2CodesObjectInputStream = null;
+		final InputStream languageTagsInputStream;
+		ObjectInputStream languageTagsObjectInputStream = null;
+
+		try {
+			startRangeIpsInputStream = classLoader.getResourceAsStream("ro/kuberam/ipLocalisation/startRangeIps.ser");
+			startRangeIpsObjectInputStream = new ObjectInputStream(startRangeIpsInputStream);
+			startRanges = (long[]) startRangeIpsObjectInputStream.readObject();
+
+			countryAlpha2CodesInputStream = classLoader.getResourceAsStream("ro/kuberam/ipLocalisation/countryIsoAlpha2Codes.ser");
+			countryAlpha2CodesObjectInputStream = new ObjectInputStream(countryAlpha2CodesInputStream);
+			countryIsoAlpha2Codes = (String[]) countryAlpha2CodesObjectInputStream.readObject();
+
+			languageTagsInputStream = classLoader.getResourceAsStream("ro/kuberam/ipLocalisation/languageTags.ser");
+			languageTagsObjectInputStream = new ObjectInputStream(languageTagsInputStream);
+			@SuppressWarnings("unchecked")
+			HashSet<String> languageTagsObject = (HashSet<String>) languageTagsObjectInputStream.readObject();
+			languageTags = languageTagsObject;
+
+			countryIsoAlpha3CodesProperties.load(classLoader.getResourceAsStream("ro/kuberam/ipLocalisation/country-iso-alpha-3-codes.properties"));
+			countryShortNamesGazetteerOrderEnUsProperties.load(classLoader.getResourceAsStream("ro/kuberam/ipLocalisation/country-short-names-gazetteer-order-en-US.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		pattern = Pattern.compile(IPADDRESS_PATTERN);
+	}
+
+	public static String getCountryAlpha2Code(String ipAddress) {
+
+		validateIpAddress(ipAddress);
+
+		long ipV4AddressAsLong = ipAddressToLong(ipAddress);
+
+		int ipRangeIndex = Arrays.binarySearch(startRanges, ipV4AddressAsLong);
+
+		ipRangeIndex = (ipRangeIndex >= 0) ? ipRangeIndex : ((-1) * ipRangeIndex) - 2;
+
+		String countryAlpha2Code = countryIsoAlpha2Codes[ipRangeIndex];
+
+		return countryAlpha2Code;
+	}
+
+	public static String getCountryAlpha3Code(String countryAlpha2Code) {
+		String countryAlpha3Code = "";
+		countryAlpha3Code = countryIsoAlpha3CodesProperties.getProperty(countryAlpha2Code, "");
+
+		if (countryAlpha3Code.equals("")) {
+			try {
+				throw new Exception(ErrorMessages.err_LOC04);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+
+		return countryAlpha3Code;
+	}
+	
+	public static String getCountryName(String countryAlpha2Code) {
+		String countryName = "";
+		countryName = countryShortNamesGazetteerOrderEnUsProperties.getProperty(countryAlpha2Code, "");
+
+		if (countryName.equals("")) {
+			try {
+				throw new Exception(ErrorMessages.err_LOC04);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+
+		return countryName;
+	}
+
+	// ancillary functions
+	protected static long ipAddressToLong(String ipAddress) {
+		String[] addrArray = ipAddress.split("\\.");
+
+		long num = 0;
+		for (int i = 0; i < addrArray.length; i++) {
+			int power = 3 - i;
+
+			num += ((Integer.parseInt(addrArray[i]) % 256 * Math.pow(256, power)));
+		}
+		return num;
+	}
+
+	protected static void validateIpAddress(String ipAddress) {
+		// InetAddress.
+		matcher = pattern.matcher(ipAddress);
+		if (!matcher.matches()) {
+			try {
+				throw new Exception(ErrorMessages.err_LOC01);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected static Boolean validateLanguageTag(String languageTag) {
+		return languageTags.contains(languageTag);
+	}
+
+}
---
+++ kuberam-maven-artifacts/ip-localisation/src/test/java/ro/kuberam/ipLocalisation/IpLocalisationFunctionsTest.java
@@ -0,0 +1,77 @@
+package ro.kuberam.ipLocalisation;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IpLocalisationFunctionsTest {
+
+	@Test
+	public void validateIpAddressTest() {
+		IpLocalisationFunctions.validateIpAddress("192.162.16.145");
+		IpLocalisationFunctions.validateIpAddress("64.233.160.0");
+	}
+
+	@Test
+	// (expected = Exception.class)
+	public void validateWrongIpAddressTest() {
+		IpLocalisationFunctions.validateIpAddress("192.162.16.1450");
+	}
+
+	@Test
+	public void ipAddressToLongTest() {
+		assertTrue(IpLocalisationFunctions.ipAddressToLong("192.162.16.145") == 3231846545L);
+		assertTrue(IpLocalisationFunctions.ipAddressToLong("64.233.160.0") == 1089052672L);
+		assertTrue(IpLocalisationFunctions.ipAddressToLong("155.140.124.174") == 2609675438L);
+	}
+
+	@Test
+	public void validateLanguageTagTest() {
+		assertTrue(IpLocalisationFunctions.validateLanguageTag("fr-FR"));
+		assertFalse(IpLocalisationFunctions.validateLanguageTag("fr-JP"));
+	}
+
+	@Test
+	public void getCountryAlpha2CodeTest() {
+		assertTrue(IpLocalisationFunctions.getCountryAlpha2Code("192.162.16.145").equals("RO"));
+		assertTrue(IpLocalisationFunctions.getCountryAlpha2Code("64.233.160.0").equals("US"));
+		assertTrue(IpLocalisationFunctions.getCountryAlpha2Code("155.140.124.174").equals("GB"));
+	}
+
+	@Test
+	public void getCountryAlpha3CodeTest() {
+		assertTrue(IpLocalisationFunctions.getCountryAlpha3Code("RO").equals("ROU"));
+		assertTrue(IpLocalisationFunctions.getCountryAlpha3Code("US").equals("USA"));
+		assertTrue(IpLocalisationFunctions.getCountryAlpha3Code("GB").equals("GBR"));
+	}
+
+	@Test
+	public void getCountryAlpha3CodeWithWrongAlpha2CodeTest() {
+		IpLocalisationFunctions.getCountryAlpha3Code("PO");
+	}
+
+	@Test
+	public void getCountryNameTest() {
+		assertTrue(IpLocalisationFunctions.getCountryName("RO").equals("Romania"));
+		assertTrue(IpLocalisationFunctions.getCountryName("US").equals("United States"));
+		assertTrue(IpLocalisationFunctions.getCountryName("GB").equals("United Kingdom"));
+	}
+
+	// public static Collection<Object[]> data() {
+	// Object[][] data = new Object[][] { { "192.102.100", false }, // must
+	// // have
+	// // a 4
+	// // '.'
+	// // characters
+	// { "a.b.c.d", false }, // you cannot have characters between '.'
+	// { "1.2.3.800", false }, // you can have only until 255
+	// { "1.2.3", false }, // you must have 4 digit parts
+	//
+	// { "192.168.1.1", true }, { "10.10.10.10", true }, { "127.0.0.1", true }
+	// };
+	//
+	// return Arrays.asList(data);
+	// }
+
+}
