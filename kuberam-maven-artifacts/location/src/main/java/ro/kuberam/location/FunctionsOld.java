98d529e92d56112c5538afd38d1bb771b4f100b2
Claudius Teodorescu <claudius.teodorescu@gmail.com>
2013-11-12 19:05:45 +0100

    Added artifact 'location'.

---
+++ kuberam-maven-artifacts/location/src/main/java/ro/kuberam/location/FunctionsOld.java
@@ -0,0 +1,10 @@
+package ro.kuberam.location;
+
+
+public class FunctionsOld {
+	
+	public String getCountryAlpha2Code(String ipAddress) {
+		return "";//GetCountryAlpha2Code.run(ipAddress);
+	}
+
+}
