f5c667c34f407656b3bdd0ccd3a2d89459d58f15
Claudius Teodorescu <claudius.teodorescu@gmail.com>
2013-12-02 12:20:50 +0100

    Added processing of @xml:base.

--- kuberam-maven-plugins/kuberam-i18n-plugin/src/test/java/ro/kuberam/maven/plugins/i18n/mojos/GenerateIpL10nDataMojoTest.java
+++ kuberam-maven-plugins/kuberam-i18n-plugin/src/test/java/ro/kuberam/maven/plugins/i18n/mojos/GenerateIpL10nDataMojoTest.java
@@ -6,16 +6,8 @@ import org.apache.maven.project.MavenProject;
 import org.junit.Test;
 
 import ro.kuberam.maven.plugins.utils.KuberamAbstractMojoTestBase;
-import ro.kuberam.maven.plugins.utils.KuberamMojoUtils;
 
 public class GenerateIpL10nDataMojoTest extends KuberamAbstractMojoTestBase {
-
-	private File ip2countryZipFile = new File(projectBuildDirectory + "ip2country.zip");
-	private File countryCodes2countryNameFile = new File(projectBuildDirectory + "countrynames.txt");
-	private File cldrZipFile = new File(projectBuildDirectory + "core.zip");
-	private File cldrFolder = new File(projectBuildDirectory + "cldr");
-	private File supplementalDataFile = new File(cldrFolder + File.separator + "common" + File.separator + "supplemental" + File.separator
-			+ "supplementalData.xml");
 
 	// @Test
 	// @Ignore
@@ -27,7 +19,7 @@ public class GenerateIpL10nDataMojoTest extends KuberamAbstractMojoTestBase {
 	// public void testSettingMojoVariables() throws Exception {
 	// GenerateIpL10nDataMojo mojo = new GenerateIpL10nDataMojo();
 	//
-	// setVariableValueToObject(mojo, "keyOne", "myValueOne");
+	//
 	//
 	// assertEquals("myValueOne", (String) getVariableValueFromObject(mojo,
 	// "keyOne"));
@@ -50,60 +42,16 @@ public class GenerateIpL10nDataMojoTest extends KuberamAbstractMojoTestBase {
 	// }
 
 	@Test
-	public void testDownloadIpToCountryDb() throws Exception {
-		KuberamMojoUtils.downloadFromUrl(GenerateIpL10nDataMojo.ip2countryDbUrl, ip2countryZipFile);
-	}
-
-	@Test
-	public void testExtractIpToCountryDb() throws Exception {
-		KuberamMojoUtils.extract(ip2countryZipFile, new File(projectBuildDirectory));
-	}
-
-	@Test
-	public void testDownloadCountryCodesToCountryNamesDb() throws Exception {
-		KuberamMojoUtils.downloadFromUrl(GenerateIpL10nDataMojo.countryCodes2countryNamesDbUrl, countryCodes2countryNameFile);
-	}
-
-	@Test
-	public void testParseIpToCountryIsoAlpha2CodeDbFunction() throws Exception {
+	public void testMojoExecute() throws Exception {
 		final GenerateIpL10nDataMojo mojo = this.mojo();
-		KuberamMojoUtils.extract(ip2countryZipFile, new File(projectBuildDirectory));
-		mojo.parseIpToCountryIsoAlpha2CodeDb(new File(projectBuildDirectory + "ip2country.db"));
-	}
-
-	@Test
-	public void testParseCountryNamesDbFunction() throws Exception {
-		final GenerateIpL10nDataMojo mojo = this.mojo();
-		mojo.parseCountryNamesDb(countryCodes2countryNameFile);
-	}
-
-	@Test
-	public void testDownloadCldrDb() throws Exception {
-		KuberamMojoUtils.downloadFromUrl(GenerateIpL10nDataMojo.cldrDbUrl, cldrZipFile);
-	}
-
-	@Test
-	public void testExtractCldrDb() throws Exception {
-		KuberamMojoUtils.extract(cldrZipFile, cldrFolder);
-	}
-
-	@Test
-	public void testParseSupaetherVersionplementalDataFileFunction() throws Exception {
-		final GenerateIpL10nDataMojo mojo = this.mojo();
-		mojo.outputDirectory = new File(projectBuildDirectory + "java");
-		mojo.parseSupplementalDataFile(supplementalDataFile);
-	}
-
-	@Test
-	public void testMojoGoal() throws Exception {
-		final GenerateIpL10nDataMojo mojo = this.mojo();
+		setVariableValueToObject(mojo, "outputDir", new File(projectBuildDirectory));
 		mojo.execute();
 	}
 
 	private GenerateIpL10nDataMojo mojo() throws Exception {
 		final GenerateIpL10nDataMojo mojo = new GenerateIpL10nDataMojo();
 
-		mojo.setProjectBuildDirectory(new File(projectBuildDirectory));
+		setVariableValueToObject(mojo, "outputDir", new File(projectBuildDirectory));
 		mojo.setProject(new MavenProject());
 		mojo.setRepoSession(newSession(newRepositorySystem()));
 
