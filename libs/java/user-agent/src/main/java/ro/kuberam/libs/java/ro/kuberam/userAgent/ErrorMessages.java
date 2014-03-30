5a8f3ad247762fd5363332ba63f89e9e3e9a3c98
claudius108 <claudius.teodorescu@gmail.com>
2014-03-31 00:52:16 +0200

    Moved user-agent lib under kuberam project.

---
+++ libs/java/user-agent/src/main/java/ro/kuberam/libs/java/ro/kuberam/userAgent/ErrorMessages.java
@@ -0,0 +1,6 @@
+package ro.kuberam.libs.java.ro.kuberam.userAgent;
+      
+public class ErrorMessages {
+      public static String err_UA01 = "err:UA01: The identificator $id is not unique.";
+      public static String err_UA02 = "err:UA02: The event $event-name does not exist.";
+}
