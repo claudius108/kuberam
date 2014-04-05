b7fce760d0af29b0fba3d9c6aac842bf1be2745d
claudius108 <claudius.teodorescu@gmail.com>
2014-04-05 18:19:15 +0200

    Made GetTextFields to return a map.

--- libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/formControls/GetTextFields.java
+++ libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/formControls/GetTextFields.java
@@ -1,14 +1,13 @@
 package ro.kuberam.libs.java.pdf.formControls;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.common.COSObjectable;
@@ -17,64 +16,46 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 
 public class GetTextFields {
 
-	private static XMLStreamWriter writer;
-
-	public static ByteArrayOutputStream run(InputStream pdfIs) throws IOException, XMLStreamException {
+	public static Map<String, String> run(InputStream pdfIs) throws IOException, XMLStreamException {
 
 		PDDocument pdfDocument = PDDocument.load(pdfIs, true);
 		PDAcroForm acroForm = pdfDocument.getDocumentCatalog().getAcroForm();
 
+		@SuppressWarnings("unchecked")
 		List<PDField> fields = acroForm.getFields();
 		Iterator<PDField> fieldsIter = fields.iterator();
 
-		XMLOutputFactory factory = XMLOutputFactory.newInstance();
-		ByteArrayOutputStream oxygenParserOutput = new ByteArrayOutputStream();
-		writer = factory.createXMLStreamWriter(oxygenParserOutput);
-
-		writer.writeStartElement("xfdf");
-		writer.writeAttribute("xmlns", "http://ns.adobe.com/xfdf/");
-		writer.writeStartElement("fields");
+		Map<String, String> result = new HashMap<String, String>();
 
 		while (fieldsIter.hasNext()) {
 			PDField field = fieldsIter.next();
-			getTextField(field, field.getPartialName());
+			getTextField(field, field.getPartialName(), result);
 		}
-
-		writer.writeEndElement();
-		writer.writeEndElement();
-		writer.flush();
 
 		pdfDocument.close();
 
-		System.out.println();
-
-		return oxygenParserOutput;
+		return result;
 	}
 
-	private static void getTextField(final PDField field, String sParent) throws IOException,
-			XMLStreamException {
+	private static void getTextField(final PDField field, String sParent, Map<String, String> result)
+			throws IOException, XMLStreamException {
 		List<COSObjectable> kids = field.getKids();
 		if (kids != null) {
 			Iterator<COSObjectable> kidsIter = kids.iterator();
 			if (!sParent.equals(field.getPartialName())) {
 				sParent = sParent + "." + field.getPartialName();
 			}
-			// System.out.println(sParent + " is of type " +
-			// field.getClass().getName());
 			while (kidsIter.hasNext()) {
 				Object pdfObj = kidsIter.next();
 				PDField kid = (PDField) pdfObj;
-				if (pdfObj instanceof PDField) {
-					getTextField(kid, sParent);
+				if (kid instanceof PDField) {
+					getTextField(kid, sParent, result);
 				}
 			}
 		} else {
-			writer.writeStartElement("field");
-			writer.writeAttribute("name", field.getFullyQualifiedName());
-			writer.writeStartElement("value");
-			writer.writeCharacters(field.getValue());
-			writer.writeEndElement();
-			writer.writeEndElement();
+			if (field.getFieldType().equals("Tx")) {
+				result.put(field.getFullyQualifiedName(), field.getValue());
+			}
 		}
 	}
 }
