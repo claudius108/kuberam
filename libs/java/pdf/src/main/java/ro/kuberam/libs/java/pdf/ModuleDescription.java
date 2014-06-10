e2efe23561d1327ea8a5a7c23ae9c438520fe0b3
claudius108 <claudius.teodorescu@gmail.com>
2014-06-10 10:03:30 +0200

    Added namespace and prefix for pdf lib.

--- libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/ModuleDescription.java
+++ libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/ModuleDescription.java
@@ -8,8 +8,8 @@ package ro.kuberam.libs.java.pdf;
  */      
 public class ModuleDescription {
       public final static String VERSION = "1.0";
-      public final static String NAMESPACE_URI = "";
-      public final static String PREFIX = "";
+      public final static String NAMESPACE_URI = "http://expath.org/ns/pdf";
+      public final static String PREFIX = "pdf";
       public final static String MODULE_NAME = "Library for EXPath PDF Module";
       public final static String MODULE_DESCRIPTION = "A module comprising a set of functions for manipulation of PDF documents";
 }
