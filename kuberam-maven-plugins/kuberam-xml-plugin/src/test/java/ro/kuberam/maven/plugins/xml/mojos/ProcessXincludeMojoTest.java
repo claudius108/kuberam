e324d9596b1396c4339bc75ad60aa43c67d8f68e
claudius108 <claudius.teodorescu@gmail.com>
2014-10-09 20:34:12 +0200

    Added support for validation with DTD.

--- kuberam-maven-plugins/kuberam-xml-plugin/src/test/java/ro/kuberam/maven/plugins/xml/mojos/ProcessXincludeMojoTest.java
+++ kuberam-maven-plugins/kuberam-xml-plugin/src/test/java/ro/kuberam/maven/plugins/xml/mojos/ProcessXincludeMojoTest.java
@@ -23,7 +23,17 @@ public class ProcessXincludeMojoTest extends KuberamAbstractMojoTestBase {
 		setVariableValueToObject(mojo, "omitXmlDeclaration", "yes");
 		setVariableValueToObject(mojo, "outputDir", new File(projectBuildDirectory + "/process-xinclude"));
 		mojo.execute();
-	}	
+	}
+	
+	public void testValidate() throws Exception {
+		final ProcessXincludeMojo mojo = this.mojo();
+		
+		setVariableValueToObject(mojo, "inputFile", new File(baseDir + File.separator + "src/test/resources/ro/kuberam/maven/plugins/xml/mojos/process-xinclude/document-02.xml"));
+		setVariableValueToObject(mojo, "omitXmlDeclaration", "yes");
+		setVariableValueToObject(mojo, "validate", "true");
+		setVariableValueToObject(mojo, "outputDir", new File(projectBuildDirectory + "/process-xinclude"));
+		mojo.execute();
+	}
 
 	private ProcessXincludeMojo mojo() throws Exception {
 		final ProcessXincludeMojo mojo = new ProcessXincludeMojo();
