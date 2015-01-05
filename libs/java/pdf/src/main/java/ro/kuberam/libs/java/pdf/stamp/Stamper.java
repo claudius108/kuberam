c47c37953b837beedf8f293efbe666d86af1bae5
Wouter Hager <wshager@gmail.com>
2015-01-05 22:34:08 +0100

    copied x,y from wrong from the interwebz...
    so the map will have to contain floats...

--- libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/stamp/Stamper.java
+++ libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/stamp/Stamper.java
@@ -18,12 +18,15 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class Stamper {
 
-    private static PDDocument pdfDocument;
-    private static int numPages;
-    private static String fontFamily = null;
-    private static Float fontSize = null;
-    private static Color nonStrokingColor = null;
-    private static Color strokingColor = null;
+	private static PDDocument pdfDocument;
+	private static int numPages;
+			// the x/y coords
+	private static Float xVal = null;
+	private staticFloat yVal = null;
+	private static String fontFamily = null;
+	private static Float fontSize = null;
+	private static Color nonStrokingColor = null;
+	private static Color strokingColor = null;
 
 	
 	public static ByteArrayOutputStream run(InputStream pdfIs, String stampString, Map<String, String> propsMap)
@@ -32,29 +35,31 @@ public class Stamper {
 	
 		pdfDocument = PDDocument.load(pdfIs, true);
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		
+		if(propsMap.containsKey("x")) xVal = Float.parseFloat(config.getProperty("x"));
+		if(propsMap.containsKey("y")) yVal = Float.parseFloat(config.getProperty("y"));
+		// make sure we have a font
+		if(propsMap.containsKey("fontFamily")) {
+			fontFamily = propsMap.get("fontFamily");
+		} else {
+			System.err.println("You must specify a font in the properties map.");
+		}
 	
-	    // make sure we have a font
-	    if(propsMap.containsKey("fontFamily")) {
-	        fontFamily = propsMap.get("fontFamily");
-	    } else {
-	        System.err.println("You must specify a font in the properties map.");
-	    }
-	
-	    // make sure we have a font size
-	    if(propsMap.containsKey("fontSize")) {
-	        fontSize = Float.parseFloat(propsMap.get("fontSize"));
-	    } else {
-	        System.err.println("You must specify a font size in the properties map.");
-	    }
-	    
-	    if(propsMap.containsKey("color")) {
-	    	nonStrokingColor = Color.decode(propsMap.get("color"));
-	    }
-	    
-	    
-	    stampPdf(stampString);
-	    
-	    pdfDocument.save(output);
+		// make sure we have a font size
+		if(propsMap.containsKey("fontSize")) {
+			fontSize = Float.parseFloat(propsMap.get("fontSize"));
+		} else {
+			System.err.println("You must specify a font size in the properties map.");
+		}
+		
+		if(propsMap.containsKey("color")) {
+			nonStrokingColor = Color.decode(propsMap.get("color"));
+		}
+		
+		
+		stampPdf(stampString);
+		
+		pdfDocument.save(output);
 		pdfDocument.close();
 	
 		return output;
@@ -66,29 +71,29 @@ public class Stamper {
 	 */
 	public static void stampPdf(String stampString) throws IOException, COSVisitorException {
 	
-	    /*if (pdfDocument.isEncrypted()) {
-	        try {
-	            // try to open the encrypted PDF
-	        	pdfDocument.decrypt("");
+		/*if (pdfDocument.isEncrypted()) {
+			try {
+				// try to open the encrypted PDF
+				pdfDocument.decrypt("");
 	
-	        } catch (InvalidPasswordException e) {
+			} catch (InvalidPasswordException e) {
 	
-	            // This error message dictates that the document is encrypted and we have no password
-	            System.err.println("The document is encrypted or otherwise has prohibitive security settings..");
-	            System.exit(1);
-	        }
-	    }*/
-	    // create the overlay page with the text to be stamped
-	    PDDocument overlayDoc = createOverlay(stampString);
+				// This error message dictates that the document is encrypted and we have no password
+				System.err.println("The document is encrypted or otherwise has prohibitive security settings..");
+				System.exit(1);
+			}
+		}*/
+		// create the overlay page with the text to be stamped
+		PDDocument overlayDoc = createOverlay(stampString);
 	
-	    // update the number of pages we have in the incoming pdf
-	    numPages = pdfDocument.getPageCount();
+		// update the number of pages we have in the incoming pdf
+		numPages = pdfDocument.getPageCount();
 	
-	    // do the overlay
-	    doOverlay(pdfDocument, overlayDoc);
+		// do the overlay
+		doOverlay(pdfDocument, overlayDoc);
 	
-	    // close
-	    overlayDoc.close();
+		// close
+		overlayDoc.close();
 	}
 	
 	/**
@@ -101,36 +106,32 @@ public class Stamper {
 	 */
 	public static PDDocument createOverlay(String text) throws IOException, COSVisitorException {
 	
-	    // Create a document and add a page to it
-	    PDDocument document = new PDDocument();
-	    PDPage page = new PDPage();
-	    document.addPage(page);
+		// Create a document and add a page to it
+		PDDocument document = new PDDocument();
+		PDPage page = new PDPage();
+		document.addPage(page);
 	
-	    // the x/y coords
-	    Float xVal = Float.parseFloat(config.getProperty("ss.xVal"));
-	    Float yVal = Float.parseFloat(config.getProperty("ss.yVal"));
+		// Create a new font object selecting one of the PDF base fonts
+		PDFont font = PDType1Font.getStandardFont(fontFamily);
 	
-	    // Create a new font object selecting one of the PDF base fonts
-	    PDFont font = PDType1Font.getStandardFont(fontFamily);
+		// Start a new content stream which will "hold" the to be created content
+		PDPageContentStream contentStream = new PDPageContentStream(document, page);
 	
-	    // Start a new content stream which will "hold" the to be created content
-	    PDPageContentStream contentStream = new PDPageContentStream(document, page);
+		// Create the text and position it
+		contentStream.beginText();
+		contentStream.setFont(font, fontSize);
+		if(nonStrokingColor != null) {
+			contentStream.setNonStrokingColor(nonStrokingColor);
+		}
+		contentStream.moveTextPositionByAmount(xVal, yVal);
+		contentStream.drawString(text);
+		contentStream.endText();
 	
-	    // Create the text and position it
-	    contentStream.beginText();
-	    contentStream.setFont(font, fontSize);
-	    if(nonStrokingColor != null) {
-	    	contentStream.setNonStrokingColor(nonStrokingColor);
-	    }
-	    contentStream.moveTextPositionByAmount(xVal, yVal);
-	    contentStream.drawString(text);
-	    contentStream.endText();
+		// Make sure that the content stream is closed:
+		contentStream.close();
 	
-	    // Make sure that the content stream is closed:
-	    contentStream.close();
-	
-	    //return the string doc
-	    return document;
+		//return the string doc
+		return document;
 	}
 	
 	/**
@@ -143,22 +144,22 @@ public class Stamper {
 	 */
 	private static void doOverlay(PDDocument basePDF, PDDocument overlayDoc) throws IOException, COSVisitorException {
 	
-	    PDDocumentCatalog docCatalog = basePDF.getDocumentCatalog();
+		PDDocumentCatalog docCatalog = basePDF.getDocumentCatalog();
 	
-	    // get the pages of the pdf
-	    List pages = docCatalog.getAllPages();
-	    Iterator pageIter = pages.iterator();
+		// get the pages of the pdf
+		List pages = docCatalog.getAllPages();
+		Iterator pageIter = pages.iterator();
 	
-	    while (pageIter.hasNext()) {
-	        PDPage page = (PDPage) pageIter.next();
-	    }
+		while (pageIter.hasNext()) {
+			PDPage page = (PDPage) pageIter.next();
+		}
 	
-	    Overlay overlay = new Overlay();
-	    PDDocument output = overlay.overlay(overlayDoc, basePDF);
-	    //overlay.overlay(overlayDoc, basePDF);
+		Overlay overlay = new Overlay();
+		PDDocument output = overlay.overlay(overlayDoc, basePDF);
+		//overlay.overlay(overlayDoc, basePDF);
 	
-	    // close, close
-	    overlayDoc.close();
-	    basePDF.close();
+		// close, close
+		overlayDoc.close();
+		basePDF.close();
 	}
 }
