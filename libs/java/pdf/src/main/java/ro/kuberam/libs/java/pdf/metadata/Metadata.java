f4a469baf0607d43854ebc7fe3752b161588fc9d
whager <whager@benjamins.nl>
2015-01-13 11:29:42 +0100

    renamend methods to get/set, return utf-8

--- libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/metadata/Metadata.java
+++ libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/metadata/Metadata.java
@@ -1,6 +1,7 @@
 package ro.kuberam.libs.java.pdf.metadata;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.xml.transform.stream.StreamResult;
@@ -9,25 +10,26 @@ import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.common.PDMetadata;
+import org.apache.pdfbox.pdmodel.common.PDStream;
 
 public class Metadata {
 
-	public static String run(InputStream pdfIs)
+	public static String get(InputStream pdfIs)
 			throws IOException, COSVisitorException {
 
 		PDDocument pdfDocument = PDDocument.load(pdfIs, true);
 		PDDocumentCatalog catalog = pdfDocument.getDocumentCatalog();
 		PDMetadata metadata = catalog.getMetadata();
-		//StreamResult result=new StreamResult(metadata.createOutputStream());
-		String result = metadata.getInputStreamAsString();
+		byte[] bStream = metadata.getByteArray();
+		String result = new String(bStream, "UTF-8");
 		pdfDocument.close();
 		
 		return result;
 	}
 	
-	public static ByteArrayOutputStream run(InputStream pdfIs, InputStream xmlmetadata)
+	public static ByteArrayOutputStream set(InputStream pdfIs, InputStream xmlmetadata)
 			throws IOException, COSVisitorException {
-
+		
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
 		
 		PDDocument pdfDocument = PDDocument.load(pdfIs, true);
@@ -35,12 +37,14 @@ public class Metadata {
 		PDMetadata metadata = catalog.getMetadata();
 
 		// write new XML metadata
-		PDMetadata newMetadata = new PDMetadata(pdfDocument, xmlmetadata, false );
+		PDMetadata newMetadata = new PDMetadata(pdfDocument, xmlmetadata, true);
 		catalog.setMetadata( newMetadata );
+		
+		//PDStream stream=new PDStream(pdfDocument);
+		//stream.addCompression();
 		
 		pdfDocument.save(output);
 		pdfDocument.close();
-		
 		return output;
 	}
 
