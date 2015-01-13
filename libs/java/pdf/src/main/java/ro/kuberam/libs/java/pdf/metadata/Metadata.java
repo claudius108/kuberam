29302a79cebe3548cc12d8d9d5893c1d3e8d35c8
whager <whager@benjamins.nl>
2015-01-13 12:24:29 +0100

    renamed xmp methods, added get-document-info method

--- libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/metadata/Metadata.java
+++ libs/java/pdf/src/main/java/ro/kuberam/libs/java/pdf/metadata/Metadata.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.lang.reflect.Method;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
@@ -13,8 +15,21 @@ import org.apache.pdfbox.pdmodel.common.PDMetadata;
 import org.apache.pdfbox.pdmodel.common.PDStream;
 
 public class Metadata {
+	
+	private static String toCamelCase(String s){
+		String[] parts = s.split("-");
+		String camelCaseString = "";
+		for (String part : parts){
+			camelCaseString = camelCaseString + toProperCase(part);
+		}
+		return camelCaseString;
+	}
 
-	public static String get(InputStream pdfIs)
+	private static String toProperCase(String s) {
+		return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+	}
+
+	public static String get-document-xmp(InputStream pdfIs)
 			throws IOException, COSVisitorException {
 
 		PDDocument pdfDocument = PDDocument.load(pdfIs, true);
@@ -27,7 +42,7 @@ public class Metadata {
 		return result;
 	}
 	
-	public static ByteArrayOutputStream set(InputStream pdfIs, InputStream xmlmetadata)
+	public static ByteArrayOutputStream set-document-xmp(InputStream pdfIs, InputStream xmlmetadata)
 			throws IOException, COSVisitorException {
 		
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -46,6 +61,29 @@ public class Metadata {
 		pdfDocument.save(output);
 		pdfDocument.close();
 		return output;
+	}
+	
+	public static String get-document-info(InputStream pdfIs, String[] properties)
+		throw IOException, COSVisitorException {
+		
+		PDDocument pdfDocument = PDDocument.load(pdfIs, true);
+		PDDocumentInformation information = pdfDocument.getDocumentInformation();
+		String[] availProps = { "author", "creator", "keywords", "producer", "subject", "title", "trapped", "creation-date", "modification-date" };
+
+		ArrayList<Object> result = new ArrayList<Object>();
+		
+		for(String prop : properties) {
+			if(availProps.contains(props)) {
+				Method method = PDDocumentInformation.class.getMethod("get" + toCamelCase(prop));
+				result.add(method.invoke(information));
+			}
+		}
+		return result;
+		/*for (Map.Entry<String, String> entry : map.entrySet()){
+			
+			Method method = PDDocumentInformation.class.getMethod("get" + toCamelCase(prop));
+			entry.getKey() entry.getValue();
+		}*/
 	}
 
 }
