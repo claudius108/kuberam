a76d9c36e840fb9893a02a9a1e4f20b506e0fb18
claudius108 <claudius.teodorescu@gmail.com>
2015-01-31 12:04:03 +0100

    Improved GetResourceMetadataTest.

--- libs/java/ft-client/src/test/java/ro/kuberam/libs/java/ftclient/FTP/GetResourceMetadataTest.java
+++ libs/java/ft-client/src/test/java/ro/kuberam/libs/java/ftclient/FTP/GetResourceMetadataTest.java
@@ -22,9 +22,17 @@ public class GetResourceMetadataTest extends FTClientAbstractTest {
 				remoteResourcePath);
 		Disconnect.disconnect(remoteConnection);
 		String resourceMetadataString = resourceMetadata.getWriter().toString();
-		String sampleResourceMetadataAsString = "<?xml version=\"1.0\" ?><ft-client:resource xmlns:ft-client=\"http://expath.org/ns/ft-client\" name=\"image-with-rights.gif\" type=\"file\" absolute-path=\"/dir-with-rights/image-with-rights.gif\" last-modified=\"2014-01-09T00:00:00+02:00\" size=\"1010\" human-readable-size=\"1010 bytes\" user=\"1003\" user-group=\"1003\" permissions=\"-rw-rw-rw-\" checksum=\"0\"></ft-client:resource>";
-		Assert.assertTrue(resourceMetadataString.equals(sampleResourceMetadataAsString));
-
+		String sampleResourceMetadataAsString = "<?xml version=\"1.0\"?><ft-client:resource xmlns:ft-client=\"http://expath.org/ns/ft-client\" name=\"image-with-rights.gif\" type=\"file\" absolute-path=\"/dir-with-rights/image-with-rights.gif\" last-modified=\"2014-01-09T00:00:00+01:00\" size=\"1010\" human-readable-size=\"1010 bytes\" user=\"1003\" user-group=\"1003\" permissions=\"-rw-rw-rw-\" checksum=\"0\"></ft-client:resource>";
+		System.out.println(resourceMetadataString);
+		Assert.assertTrue(resourceMetadataString.contains("name=\"image-with-rights.gif\""));
+		Assert.assertTrue(resourceMetadataString.contains("type=\"file\""));
+		Assert.assertTrue(resourceMetadataString.contains("absolute-path=\"/dir-with-rights/image-with-rights.gif\""));
+		Assert.assertTrue(resourceMetadataString.contains("size=\"1010\""));
+		Assert.assertTrue(resourceMetadataString.contains("human-readable-size=\"1010 bytes\""));
+		Assert.assertTrue(resourceMetadataString.contains("user=\"1003\""));
+		Assert.assertTrue(resourceMetadataString.contains("user-group=\"1003\""));
+		Assert.assertTrue(resourceMetadataString.contains("permissions=\"-rw-rw-rw-\""));
+		Assert.assertTrue(resourceMetadataString.contains("checksum=\"0\""));
 	}
 
 }
