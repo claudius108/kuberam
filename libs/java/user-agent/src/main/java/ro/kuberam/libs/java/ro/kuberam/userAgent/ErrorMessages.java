7eca9f1730a09d6e33ec221f9bfdb90fcc4dcc25
claudius108 <claudius.teodorescu@gmail.com>
2015-02-24 16:29:15 +0100

    Generated the 'ua' spec.

--- libs/java/user-agent/src/main/java/ro/kuberam/libs/java/ro/kuberam/userAgent/ErrorMessages.java
+++ libs/java/user-agent/src/main/java/ro/kuberam/libs/java/ro/kuberam/userAgent/ErrorMessages.java
@@ -5,5 +5,5 @@ public class ErrorMessages {
       public static String err_UA02 = "err:UA02: The event $event-name does not exist.";
       public static String err_UA03 = "err:UA03: The identificator $id does not exist.";
       public static String err_UA04 = "err:UA04: No node was selected.";
-      public static String err_UA05 = "err:UA05: The identificator $id does not exist.";
+      public static String err_UA05 = "err:UA05: The template $template-id does not exist.";
 }
