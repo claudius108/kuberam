2a7daa2565159b74affe590d72e06f97695ebbbf
claudius108 <claudius.teodorescu@gmail.com>
2015-02-27 23:12:16 +0100

    Moved 'kuberam-maven-plugins' to separate repos.

--- libs/java/crypto/src/main/java/ro/kuberam/libs/java/crypto/ErrorMessages.java
+++ libs/java/crypto/src/main/java/ro/kuberam/libs/java/crypto/ErrorMessages.java
@@ -1,22 +1,20 @@
 package ro.kuberam.libs.java.crypto;
       
 public class ErrorMessages {
-      public static String crypto_canonicalization-algorithm-error = "crypto:canonicalization-algorithm-error: The canonicalization algorithm is not supported.";
-      public static String crypto_digest-algorithm-error = "crypto:digest-algorithm-error: The digest algorithm is not supported.";
-      public static String crypto_signature-algorithm-error = "crypto:signature-algorithm-error: The signature algorithm is not supported.";
-      public static String crypto_error-reading-keystore = "crypto:error-reading-keystore: I/O error while reading keystore, or the password is incorrect.";
-      public static String crypto_denied-reading-keystore = "crypto:denied-reading-keystore: Permission denied to read keystore.";
-      public static String crypto_invalid-keystore-url = "crypto:invalid-keystore-url: The keystore URL is invalid.";
-      public static String crypto_keystore-type-error = "crypto:keystore-type-error: The keystore type is not supported.";
-      public static String crypto_no-key-for-alias = "crypto:no-key-for-alias: Cannot find key for alias in given keystore.";
-      public static String crypto_signature-element-error = "crypto:signature-element-error: Cannot find Signature element.";
-      public static String crypto_padding-error = "crypto:padding-error: No such padding.";
+      public static String crypto_unknown-algorithm = "crypto:unknown-algorithm: The specified algorithm is not supported.";
+      public static String crypto_signature-type = "crypto:signature-type: The specified signature type is not supported.";
+      public static String crypto_unreadable-keystore = "crypto:unreadable-keystore: I/O error while reading keystore, or the password is incorrect.";
+      public static String crypto_denied-keystore = "crypto:denied-keystore: Permission denied to read keystore.";
+      public static String crypto_keystore-url = "crypto:keystore-url: The keystore URL is invalid.";
+      public static String crypto_keystore-type = "crypto:keystore-type: The keystore type is not supported.";
+      public static String crypto_alias-key = "crypto:alias-key: Cannot find key for alias in given keystore.";
+      public static String crypto_signature-element = "crypto:signature-element: Cannot find Signature element.";
+      public static String crypto_inexistent-padding = "crypto:inexistent-padding: No such padding.";
       public static String crypto_incorrect-padding = "crypto:incorrect-padding: Incorrect padding.";
-      public static String crypto_encryption-type-error = "crypto:encryption-type-error: The encryption type is not supported.";
-      public static String crypto_invalid-secret-key = "crypto:invalid-secret-key: The secret key is invalid.";
-      public static String crypto_block-size-error = "crypto:block-size-error: Illegal block size.";
-      public static String crypto_algorithm-or-transformation-error = "crypto:algorithm-or-transformation-error: The algorithm or transformation is not supported.";
-      public static String crypto_decryption-type-error = "crypto:decryption-type-error: The decryption type is not supported.";
+      public static String crypto_encryption-type = "crypto:encryption-type: The encryption type is not supported.";
+      public static String crypto_ecret-key = "crypto:ecret-key: The secret key is invalid.";
+      public static String crypto_block-size = "crypto:block-size: Illegal block size.";
+      public static String crypto_decryption-type = "crypto:decryption-type: The decryption type is not supported.";
       public static String crypto_no-provider = "crypto:no-provider: The provider is not set.";
-      public static String crypto.output-format-error = "crypto.output-format-error: The output format is not supported.";
+      public static String crypto.output-format = "crypto.output-format: The output format is not supported.";
 }
